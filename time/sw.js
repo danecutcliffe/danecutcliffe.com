ac1d0bb6d287366219fad2dc09b13b0cbbc0f838
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-18 19:13:26 -0300

    Calculate overtime by PEI work week

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v3';
+const CACHE_NAME = 'time-clock-runtime-v4';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
