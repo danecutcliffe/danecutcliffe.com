171d0e422272274bd3947de464c110b146e9882c
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-21 12:57:32 -0300

    Fix timesheet edit button sizing

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v8';
+const CACHE_NAME = 'time-clock-runtime-v9';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
