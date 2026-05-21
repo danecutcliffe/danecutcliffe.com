77b008292081a3fbd8feee46c3f09cf2850091d3
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-21 14:54:04 -0300

    Simplify daily break breakdown

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v15';
+const CACHE_NAME = 'time-clock-runtime-v16';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
