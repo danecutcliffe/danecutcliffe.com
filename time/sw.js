cf0989dbd1bb262c6dbc9cc2842549dbbd99c09d
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-21 14:10:43 -0300

    Cap paid lunch by day

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v11';
+const CACHE_NAME = 'time-clock-runtime-v12';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
