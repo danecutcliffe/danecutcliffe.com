4a8e0f7b066285642c5fd2036b9100cedac7ed35
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-21 14:18:13 -0300

    Group admin timesheets by day

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v12';
+const CACHE_NAME = 'time-clock-runtime-v13';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
