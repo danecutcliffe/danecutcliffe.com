81bfecde88caa49a27d585d62dba4a6e935b7101
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-22 16:58:41 -0300

    Refine passkey setup flow

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v23';
+const CACHE_NAME = 'time-clock-runtime-v26';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
