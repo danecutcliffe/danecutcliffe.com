4c4a40a7c233d1fa361be6b4bd24dbb239b6e8f3
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-22 14:56:51 -0300

    Stabilize mobile nav and day separators

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v20';
+const CACHE_NAME = 'time-clock-runtime-v22';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
