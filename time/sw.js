13d2834edae0aa64d0e07281cd1c5f2aa8cc2fa0
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-22 17:22:12 -0300

    Refine admin timesheet approval UI

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v26';
+const CACHE_NAME = 'time-clock-runtime-v27';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
