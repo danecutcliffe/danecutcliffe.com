5eda1a98eafda07a670d9b0270a596e035d52070
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-26 09:09:38 -0300

    Add admin punch log view

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v29';
+const CACHE_NAME = 'time-clock-runtime-v30';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
