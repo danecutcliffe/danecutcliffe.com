24c639c79b468afe9522c2ababb0fd2c9bc3082f
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-05-27 11:32:45 -0300

    Deploy manual break entry updates

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-v31';
+const CACHE_NAME = 'time-clock-runtime-v32';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
