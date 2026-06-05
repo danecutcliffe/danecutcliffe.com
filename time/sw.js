23c4de998a378f29fc793f17542f5de33cfab460
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-05 10:00:49 -0300

    chore: refresh time service worker stamp

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260605130000';
+const CACHE_NAME = 'time-clock-runtime-20260605130010';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
